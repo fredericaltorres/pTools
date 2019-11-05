8e206a6e079b66fa04e90d5cec9e9a6ad4b4923f
Frederic Torres <fredericaltorres@gmail.com>
2019-11-05 10:58:10 -0500

    Add more search options

--- ff.ps1
+++ ff.ps1
@@ -9,24 +9,45 @@
 param(
     [Parameter(Mandatory=$true)] 
     [Alias('f')]
-    [string]$wildcard,
+    #[string]
+    $wildcard,
 
     [Parameter(Mandatory=$false)]
     [Alias('s')]
-    [string]$searchForRegEx = $null,
+    [string]$searchForRegEx = "",
 
     [Parameter(Mandatory=$false)]
     [Alias('p')]
-    [string]$path = "."
+    [string]$path = ".",
+
+    [Parameter(Mandatory=$false)]
+    [Alias('e')]
+    $exclude = @(),
+
+    [Parameter(Mandatory=$false)]
+    [Alias('eb')]
+    [bool]$excludeBinary = $false
 )
+if($excludeBinary) {
+    $exclude = $exclude + @("*.exe","*.pdb","*.dll", "node_modules", "*.bak")
+}
+if($wildcard -eq "dotnet") {
+    $wildcard = @("*.cs", "*.aspx", "*.ascx", "*.asax")
+}
+if($wildcard -eq "vb6") {
+    $wildcard = @("*.cls", "*.bas", "*.vbp")
+}
 $scriptTitle = "FindFile ff"
 # Write-Host "searchForRegEx:$searchForRegEx, IsNull:$($null -eq $searchForRegEx)"
 
-if($searchForRegEx -eq $null) {
+if($searchForRegEx -eq "") {
     Write-Host "$scriptTitle - wildcard: $wildcard"
     Get-ChildItem -path $path -rec -include $wildcard
 }
 else {
-    Write-Host "$scriptTitle - wildcard: $wildcard - search: $searchForRegEx"
-    Get-ChildItem -path $path -rec -include $wildcard | select-string $searchForRegEx
+    Write-Host "$scriptTitle - wildcard:'$wildcard' exclude:'$exclude' search:'$searchForRegEx'"
+    if($path -ne ".") {
+        Write-Host "path:'$path'"
+    }
+    Get-ChildItem -path $path -rec -include $wildcard -Exclude $exclude | select-string $searchForRegEx -list
 }
