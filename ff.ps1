a90dc8cb7801172ad534da8216295dea3166dd3c
Frederic Torres <fredericaltorres@gmail.com>
2019-11-16 13:33:46 -0500

    Implement search/replace

--- ff.ps1
+++ ff.ps1
@@ -7,14 +7,18 @@
 #>
 [CmdletBinding()]
 param(
-    [Parameter(Mandatory=$true)] 
+    [Parameter(Mandatory=$true)] ## $true
     [Alias('f')]
-    #[ValidateSet('dotnet', 'vb6')]
+    #[ValidateSet('dotnet', 'vb6', 'allcode')]
     $wildcard,
     
     [Parameter(Mandatory=$false)]
     [Alias('s')]
     [string]$searchForRegEx = "",
+
+    [Parameter(Mandatory=$false)]
+    [Alias('r')]
+    [string]$replace = "",
 
     [Parameter(Mandatory=$false)]
     [Alias('p')]
@@ -28,14 +32,22 @@ param(
     [Alias('eb')]
     [bool]$excludeBinary = $false
 )
+$dotNetFileExtensions = @("*.cs", "*.aspx", "*.ascx", "*.asax", "*.csproj", "*.sln")
+$vb6FileExtensions = @("*.cls", "*.bas", "*.vbp")
+$classicAspFileExtensions = @("*.asp", "*.vbs")
+$javaScriptFrontEndFileExtensions = @("*.js", "*.ts", "*.json")
+
 if($excludeBinary) {
     $exclude = $exclude + @("*.exe","*.pdb","*.dll", "node_modules", "*.bak")
 }
 if($wildcard -eq "dotnet") {
-    $wildcard = @("*.cs", "*.aspx", "*.ascx", "*.asax", "*.csproj", "*.sln")
+    $wildcard = $dotNetFileExtensions
 }
 if($wildcard -eq "vb6") {
-    $wildcard = @("*.cls", "*.bas", "*.vbp")
+    $wildcard = $vb6FileExtensions
+}
+if($wildcard -eq "allcode") {
+    $wildcard = $vb6FileExtensions + $dotNetFileExtensions + $classicAspFileExtensions + $javaScriptFrontEndFileExtensions
 }
 
 $scriptTitle = "FindFile ff"
@@ -44,14 +56,31 @@ if($exclude.length -eq 0) {
     $exclude = $null
 }
 
-if($searchForRegEx -eq "") {
-    Write-Host "$scriptTitle - wildcard: $wildcard"
-    Get-ChildItem -path $path -rec -include $wildcard -exclude $exclude 
+if($replace -eq "") {
+
+    if($searchForRegEx -eq "") {
+        Write-Host "$scriptTitle - wildcard: $wildcard"
+        Get-ChildItem -path $path -rec -include $wildcard -exclude $exclude 
+    }
+    else {
+        Write-Host "$scriptTitle - wildcard:'$wildcard' exclude:'$exclude' search:'$searchForRegEx'"
+        if($path -ne ".") {
+            Write-Host "path:'$path'"
+        }
+        Get-ChildItem -path $path -rec -include $wildcard -exclude $exclude | select-string $searchForRegEx -list 
+    }
 }
 else {
-    Write-Host "$scriptTitle - wildcard:'$wildcard' exclude:'$exclude' search:'$searchForRegEx'"
-    if($path -ne ".") {
-        Write-Host "path:'$path'"
+Write-Host "$scriptTitle - wildcard:'$wildcard' exclude:'$exclude' search:'$searchForRegEx' replace:'$replace'"
+    $files = Get-ChildItem -path $path -rec -include $wildcard -exclude $exclude
+    foreach ($file in $files)
+    {
+        $content = Get-Content $file.PSPath
+        if($content -match $searchForRegEx) {
+
+            Write-Host "Updating file $file"
+            $content = $content -replace $searchForRegEx, $replace         
+            $content | Set-Content $file.PSPath
+        }
     }
-    Get-ChildItem -path $path -rec -include $wildcard -exclude $exclude | select-string $searchForRegEx -list 
 }
